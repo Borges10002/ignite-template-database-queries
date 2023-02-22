0104a2499b48cfbea3bd32563745c27e4cab419c
borges10002 <borges10002@gmail.com>
2023-02-21 21:50:53 -0400

    m

---
+++ rockeset (1).sql
@@ -0,0 +1,55 @@
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "fist_name" string,
+  "last_name" string,
+  "email" string,
+  "create_at" date,
+  "update_at" date
+);
+
+CREATE TABLE "users_x_orders" (
+  "users_id" uuid,
+  "orders_id" uuid
+);
+
+CREATE TABLE "orders" (
+  "id" SERIAL PRIMARY KEY,
+  "create_at" date,
+  "update_at" date
+);
+
+CREATE TABLE "orders_x_games" (
+  "orders_id" uuid,
+  "games_id" uuid
+);
+
+CREATE TABLE "games" (
+  "id" SERIAL PRIMARY KEY,
+  "title" string,
+  "create_at" date,
+  "update_at" date
+);
+
+CREATE TABLE "games_x_geners" (
+  "games_id" uuid,
+  "geners_id" uuid
+);
+
+CREATE TABLE "geners" (
+  "id" SERIAL PRIMARY KEY,
+  "name" string,
+  "create_at" date,
+  "update_at" date
+);
+
+ALTER TABLE "users_x_orders" ADD FOREIGN KEY ("users_id") REFERENCES "users" ("id");
+
+ALTER TABLE "orders" ADD FOREIGN KEY ("id") REFERENCES "users_x_orders" ("orders_id");
+
+ALTER TABLE "orders_x_games" ADD FOREIGN KEY ("orders_id") REFERENCES "orders" ("id");
+
+ALTER TABLE "games" ADD FOREIGN KEY ("id") REFERENCES "orders_x_games" ("games_id");
+
+ALTER TABLE "games_x_geners" ADD FOREIGN KEY ("games_id") REFERENCES "games" ("id");
+
+ALTER TABLE "geners" ADD FOREIGN KEY ("id") REFERENCES "games_x_geners" ("geners_id");
